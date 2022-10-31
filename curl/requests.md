7b06b6924911bee5c61d5e065eb40895b231cdcf
Denis Popkov <pkvdenis@gmail.com>
2022-10-31 17:29:16 +0300

    security: UserRestController: implement login.

--- curl/requests.md
+++ curl/requests.md
@@ -2,3 +2,8 @@
 ```shell script
 curl -X POST -H 'Content-Type: application/json' -d @register-data.json http://localhost:8080/user/register
 ```
+
+## Login using username and password
+```shell script
+curl -X POST -H 'Content-Type: application/json' -d @login-data.json http://localhost:8080/user/login
+```
--- src/main/java/io/dpopkov/knowthenix/ums/rest/UserRestController.java
+++ src/main/java/io/dpopkov/knowthenix/ums/rest/UserRestController.java
@@ -1,10 +1,15 @@
 package io.dpopkov.knowthenix.ums.rest;
 
+import io.dpopkov.knowthenix.ums.constants.SecurityConstants;
 import io.dpopkov.knowthenix.ums.domain.AuthUser;
+import io.dpopkov.knowthenix.ums.domain.UserPrincipal;
 import io.dpopkov.knowthenix.ums.exceptions.domain.AppDomainException;
 import io.dpopkov.knowthenix.ums.services.AuthUserService;
+import io.dpopkov.knowthenix.ums.utility.JwtProvider;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +17,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserRestController {
 
     private final AuthUserService authUserService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
 
-    public UserRestController(AuthUserService authUserService) {
+    public UserRestController(AuthUserService authUserService,
+                              AuthenticationManager authenticationManager,
+                              JwtProvider jwtProvider) {
         this.authUserService = authUserService;
+        this.authenticationManager = authenticationManager;
+        this.jwtProvider = jwtProvider;
     }
 
     @GetMapping("/home")
@@ -28,5 +39,26 @@ public class UserRestController {
                 user.getUsername(), user.getEmail());
         // todo: fix this line below - the actual entity should not be sent as response!
         return new ResponseEntity<>(registered, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthUser> login(@RequestBody AuthUser user) {
+        authenticate(user.getUsername(), user.getPassword());
+        AuthUser loginUser = authUserService.findByUsername(user.getUsername()).orElseThrow();
+        UserPrincipal principal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = createJwtHeader(principal);
+        // todo: fix this line below - the actual entity should not be sent as response!
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private void authenticate(String username, String password)
+            throws DisabledException, LockedException, BadCredentialsException {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
+    private HttpHeaders createJwtHeader(UserPrincipal principal) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(SecurityConstants.JWT_HEADER, jwtProvider.generateToken(principal));
+        return headers;
     }
 }
