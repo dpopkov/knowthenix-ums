0feb8e0cd4713ab6970c274a37f84930a6e6a351
Denis Popkov <pkvdenis@gmail.com>
2022-10-31 17:18:42 +0300

    security: fix error with AuthenticationManager in SecurityConfig.

--- src/main/java/io/dpopkov/knowthenix/ums/config/SecurityConfig.java
+++ src/main/java/io/dpopkov/knowthenix/ums/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     @Override
-    public AuthenticationManager authenticationManager() throws Exception {
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 }
